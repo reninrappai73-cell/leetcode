5e469f32ebe3f3b4d4c51e10c6b15a2492e22e28
reninrappai73-cell <reninrappai73@gmail.com>
2026-03-19 11:13:18 +0530

    Time: 1232 ms (46.22%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
-select product_name,year,price 
+-- Write your PostgreSQL query statement below
+select p.product_name,
+s.year,
+s.price
 from sales s
-inner join product p on s.product_id = p.product_id;
+join product p on s.product_id = p.product_id;
